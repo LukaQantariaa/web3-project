117b556b2fd0e445462e237c90993eafa280901c
Luka Kantaria <l.kantaria@adjarabet.com>
2019-11-05 18:04:22 +0400

    added login page

--- project-front/src/app/app-routing.module.ts
+++ project-front/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: "login", loadChildren: () => import("./modules/login/login.module").then(m => m.LoginModule)}
+  {path:"login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project-front/src/app/app.module.ts
+++ project-front/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './modules/login/login.module';
+import { LoginComponent } from './modules/login/login.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
